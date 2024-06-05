853590cee2858c45f04ea311d6dae405f0b801f5
alfie-ns <alfienurse@gmail.com>
2024-06-05 20:51:10 +0100

    update

---
+++ pbd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Save the current directory name
+current_dir=$(basename "$PWD")
+
+# Run the push script and ensure it completes successfully
+./push.sh && cd .. && rm -rf "$current_dir"
+
+# Streamline the process of deleting the repo only after push is finished
