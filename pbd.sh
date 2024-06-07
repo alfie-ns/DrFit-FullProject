3dd830a0a9524084e7d6dcc8c1fe8dc7893445de
alfie-ns <alfienurse@gmail.com>
2024-06-07 08:56:29 +0100

    update

--- pbd.sh
+++ pbd.sh
@@ -3,8 +3,14 @@
 current_dir=$(basename "$PWD")
 
 # Run the push script and ensure it completes successfully
-./push.sh && cd .. && rm -rf "$current_dir"
-
+if ./push.sh; then
+    # If the push script completes successfully, delete the repository
+    cd ..
+    rm -rf $current_dir
+else
+    # If the push script fails, do not delete the repository
+    echo "Push failed. Repository not deleted."
+fi
 # Streamline the process of deleting the repo only after push is finished
 
 # Push Backout Delete (PBD) script
